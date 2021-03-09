c122ea9c466e6990ffb6be88d0142f118584d338
Pradeep Sahani <mark42virus@gmail.com>
2021-03-09 07:36:31 -0800

    updating files

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -5,12 +5,12 @@ const router = express.Router();
 const adminHandler = require('../controller/adminController');
 const tokenVerify = require('../jwt_data/jwtVerify');
 
-router.post('/register', adminHandler.adminReg);
-router.post('/login', adminHandler.adminLogin);
-router.get('/profile', tokenVerify.tokenValidation, adminHandler.adminProfile);
+router.post('/register', adminHandler.adminReg); // admin registration - only admin purpose
+router.post('/login', adminHandler.adminLogin); // admin login
+router.get('/profile', tokenVerify.tokenValidation, adminHandler.adminProfile); // admin profile api
 router.get('/allUsers', tokenVerify.tokenValidation, adminHandler.findallUser); // getting all users in the admin block
-router.put('/updateUser/:id', tokenVerify.tokenValidation, adminHandler.updateUser);
-router.delete('/deleteUser/:id', tokenVerify.tokenValidation, adminHandler.deleteUser);
+router.put('/updateUser/:id', tokenVerify.tokenValidation, adminHandler.updateUser); // updating user data
+router.delete('/deleteUser/:id', tokenVerify.tokenValidation, adminHandler.deleteUser); // deleting user data
 
 
 
